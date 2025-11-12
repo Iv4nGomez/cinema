a6916a95b98ce440b6cca1449db7382fffc1d163
Iv4nGomez <xigomjim596@ieshnosmachado.org>
2025-11-12 14:51:25 +0100

    Add modal function correct

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 button = document.getElementById('button-trailer')
 
+div = document.getElementById('modal')
+buttonExit = document.getElementById('exit-message')
+video = document.getElementById('video')
+
 button.addEventListener('click', () => {
-    div = document.getElementById('modal')
     div.classList.remove('hidden')
+    buttonExit.classList.remove('hidden')
+    video.classList.remove('hidden')
     div.classList.add('show-modal')
     main = document.querySelector('main');
     header = document.querySelector('header');
@@ -12,6 +17,22 @@ button.addEventListener('click', () => {
     footer.classList.add('blur-background');
     hero = document.getElementById('hero');
     hero.classList.add('blur-background')
+})
+
+
+buttonExit.addEventListener('click', () => {
+    div.classList.add('hidden')
+    buttonExit.classList.add('hidden')
+    video.classList.add('hidden')
+    div.classList.remove('show-modal')
+    main = document.querySelector('main');
+    header = document.querySelector('header');
+    footer = document.querySelector('footer')
+    main.classList.remove('blur-background');
+    header.classList.remove('blur-background');
+    footer.classList.remove('blur-background');
+    hero = document.getElementById('hero');
+    hero.classList.remove('blur-background')
 })
 
 
